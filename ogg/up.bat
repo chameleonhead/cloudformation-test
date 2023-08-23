8d60c8e696731d45e1145626b78de1296c0c0c60
Yuto Nagano <chamelion@hotmail.co.jp>
2023-08-23 12:03:15 +0900

    命名を変更

--- ogg/up.bat
+++ ogg/up.bat
@@ -1,10 +1,10 @@
-aws cloudformation create-stack --stack-name vpc-intra --template-body file://vpc-intra.yml
-aws cloudformation wait stack-create-complete --stack-name vpc-intra
-aws cloudformation create-stack --stack-name vpc-intra-resources --template-body file://vpc-intra-resources.yml --capabilities CAPABILITY_IAM
-aws cloudformation wait stack-create-complete --stack-name vpc-intra-resources
-aws cloudformation create-stack --stack-name vpc-multiple-cidr --template-body file://vpc-multiple-cidr.yml
-aws cloudformation wait stack-create-complete --stack-name vpc-multiple-cidr
-aws cloudformation create-stack --stack-name vpc-multiple-cidr-intra-connection --template-body file://vpc-multiple-cidr-intra-connection.yml
-aws cloudformation wait stack-create-complete --stack-name vpc-multiple-cidr-intra-connection
-aws cloudformation create-stack --stack-name vpc-multiple-cidr-resources --template-body file://vpc-multiple-cidr-resources.yml
-aws cloudformation wait stack-create-complete --stack-name vpc-multiple-cidr-resources
+aws cloudformation create-stack --stack-name intra-vpc --template-body file://intra-vpc.yml
+aws cloudformation wait stack-create-complete --stack-name intra-vpc
+aws cloudformation create-stack --stack-name intra-resources --template-body file://intra-resources.yml --capabilities CAPABILITY_IAM
+aws cloudformation wait stack-create-complete --stack-name intra-resources
+aws cloudformation create-stack --stack-name multicidr-vpc --template-body file://multicidr-vpc.yml
+aws cloudformation wait stack-create-complete --stack-name multicidr-vpc
+aws cloudformation create-stack --stack-name multicidr-intra-connection --template-body file://multicidr-intra-connection.yml
+aws cloudformation wait stack-create-complete --stack-name multicidr-intra-connection
+aws cloudformation create-stack --stack-name multicidr-resources --template-body file://multicidr-resources.yml
+aws cloudformation wait stack-create-complete --stack-name multicidr-resources
