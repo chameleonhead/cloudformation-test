6c89de7fcd270a8818760f9417e853bf81246d8a
Yuto Nagano <chamelion@hotmail.co.jp>
2023-08-23 13:35:18 +0900

    transit gatewayだけ別ファイルに変更

--- ogg/down.bat
+++ ogg/down.bat
@@ -1,10 +1,12 @@
-aws cloudformation delete-stack --stack-name vpc-multiple-cidr-resources
-aws cloudformation wait stack-delete-complete --stack-name vpc-multiple-cidr-resources
-aws cloudformation delete-stack --stack-name vpc-multiple-cidr-intra-connection
-aws cloudformation wait stack-delete-complete --stack-name vpc-multiple-cidr-intra-connection
-aws cloudformation delete-stack --stack-name vpc-multiple-cidr
-aws cloudformation wait stack-delete-complete --stack-name vpc-multiple-cidr
+aws cloudformation delete-stack --stack-name multicidr-resources
 aws cloudformation delete-stack --stack-name vpc-intra-resources
+aws cloudformation wait stack-delete-complete --stack-name multicidr-resources
 aws cloudformation wait stack-delete-complete --stack-name vpc-intra-resources
+
+aws cloudformation delete-stack --stack-name tgw
+aws cloudformation wait stack-delete-complete --stack-name tgw
+
+aws cloudformation delete-stack --stack-name multicidr-vpc
 aws cloudformation delete-stack --stack-name vpc-intra
+aws cloudformation wait stack-delete-complete --stack-name multicidr-vpc
 aws cloudformation wait stack-delete-complete --stack-name vpc-intra
--- ogg/up.bat
+++ ogg/up.bat
@@ -1,10 +1,12 @@
 aws cloudformation create-stack --stack-name intra-vpc --template-body file://intra-vpc.yml
-aws cloudformation wait stack-create-complete --stack-name intra-vpc
-aws cloudformation create-stack --stack-name intra-resources --template-body file://intra-resources.yml --capabilities CAPABILITY_IAM
-aws cloudformation wait stack-create-complete --stack-name intra-resources
 aws cloudformation create-stack --stack-name multicidr-vpc --template-body file://multicidr-vpc.yml
+aws cloudformation wait stack-create-complete --stack-name intra-vpc
 aws cloudformation wait stack-create-complete --stack-name multicidr-vpc
-aws cloudformation create-stack --stack-name multicidr-intra-connection --template-body file://multicidr-intra-connection.yml
-aws cloudformation wait stack-create-complete --stack-name multicidr-intra-connection
+
+aws cloudformation create-stack --stack-name tgw --template-body file://tgw.yml
+aws cloudformation wait stack-create-complete --stack-name tgw
+
+aws cloudformation create-stack --stack-name intra-resources --template-body file://intra-resources.yml --capabilities CAPABILITY_IAM
 aws cloudformation create-stack --stack-name multicidr-resources --template-body file://multicidr-resources.yml
+aws cloudformation wait stack-create-complete --stack-name intra-resources
 aws cloudformation wait stack-create-complete --stack-name multicidr-resources
