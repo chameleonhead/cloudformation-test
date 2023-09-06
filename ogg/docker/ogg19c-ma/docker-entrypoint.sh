dae4c6f6c28988c3bb3dd776758a72482c30d67f
Yuto Nagano <chamelion@hotmail.co.jp>
2023-09-06 22:07:48 +0900

    OGG HUBとしてレプリケーションを設定

--- ogg/docker/ogg19c-ma/docker-entrypoint.sh
+++ ogg/docker/ogg19c-ma/docker-entrypoint.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 if [ ! -e /opt/oracle/ogg_deployments/Local ]; then
+    sed -i "s/11000/$PORT_BASE/" /tmp/orainstall/oggca.rsp
+    sed -i "s/11001/$(expr ${PORT_BASE} + 1)/" /tmp/orainstall/oggca.rsp
+    sed -i "s/11002/$(expr ${PORT_BASE} + 2)/" /tmp/orainstall/oggca.rsp
+    sed -i "s/11003/$(expr ${PORT_BASE} + 3)/" /tmp/orainstall/oggca.rsp
+    sed -i "s/11004/$(expr ${PORT_BASE} + 4)/" /tmp/orainstall/oggca.rsp
+    sed -i "s/11005/$(expr ${PORT_BASE} + 5)/" /tmp/orainstall/oggca.rsp
     /opt/oracle/product/19.1.0/oggcore_1/bin/oggca.sh -silent -responseFile /tmp/orainstall/oggca.rsp
 fi;
 
-export DEPLOYMENT_BASE=/opt/oracle/ogg_deployments/Local
-export OGG_ETC_HOME=/opt/oracle/ogg_deployments/Local/ServiceManager/etc
-export OGG_VAR_HOME=/opt/oracle/ogg_deployments/Local/ServiceManager/var
-$DEPLOYMENT_BASE/ServiceManager/bin/startSM.sh
-trap "$DEPLOYMENT_BASE/ServiceManager/bin/stopSM.sh; exit" SIGINT SIGTERM
+export DEPLOYMENT_BASE=/opt/oracle/ogg_deployments/ServiceManager
+export OGG_ETC_HOME=$DEPLOYMENT_BASE/etc
+export OGG_VAR_HOME=$DEPLOYMENT_BASE/var
+$DEPLOYMENT_BASE/bin/startSM.sh
+trap "$DEPLOYMENT_BASE/bin/stopSM.sh; exit" SIGINT SIGTERM
 tail -f $OGG_VAR_HOME/log/*.log
