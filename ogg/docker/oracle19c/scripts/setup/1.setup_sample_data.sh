fbd455edb136550a357bc9f5f0f83775dec9979b
Yuto Nagano <chamelion@hotmail.co.jp>
2023-09-06 18:20:47 +0900

    スクリプトの実行パスを修正

--- ogg/docker/oracle19c/scripts/setup/1.setup_sample_data.sh
+++ ogg/docker/oracle19c/scripts/setup/1.setup_sample_data.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-ORIGPATH=$(pwd)
+#!/usr/bin/env bash
+ORIGPATH=$(dirname "$0")
 unzip -o $ORIGPATH/db-sample-schemas-main.zip -d $ORIGPATH
 cd $ORIGPATH/db-sample-schemas-main/human_resources
 sqlplus / as sysdba @$ORIGPATH/setup_sample_data.dat << EOF
