56803d0b0c25199dc6fb4c99180ee7f746de7e24
Yuto Nagano <chamelion@hotmail.co.jp>
2023-09-06 18:17:16 +0900

    OGGのセットアップコードを修正

--- ogg/docker/ogg19c-ma/docker-entrypoint.sh
+++ ogg/docker/ogg19c-ma/docker-entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ ! -e /opt/oracle/ogg_deployments/Local ]; then
+    /opt/oracle/product/19.1.0/oggcore_1/bin/oggca.sh -silent -responseFile /tmp/orainstall/oggca.rsp
+fi;
+
 export DEPLOYMENT_BASE=/opt/oracle/ogg_deployments/Local
 export OGG_ETC_HOME=/opt/oracle/ogg_deployments/Local/ServiceManager/etc
 export OGG_VAR_HOME=/opt/oracle/ogg_deployments/Local/ServiceManager/var
--- ogg/docker/oracle19c/scripts/setup/1.setup_sample_data.sh
+++ ogg/docker/oracle19c/scripts/setup/1.setup_sample_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ORIGPATH=$(pwd)
-unzip -o ./db-sample-schemas-main.zip -d $ORIGPATH
+unzip -o $ORIGPATH/db-sample-schemas-main.zip -d $ORIGPATH
 cd $ORIGPATH/db-sample-schemas-main/human_resources
 sqlplus / as sysdba @$ORIGPATH/setup_sample_data.dat << EOF
 
