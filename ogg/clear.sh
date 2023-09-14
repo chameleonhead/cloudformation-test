3ad0d14f0f14255d0553e4c608bd9e184da129c0
Yuto Nagano <chamelion@hotmail.co.jp>
2023-09-14 09:28:28 +0900

    データのクリア処理を記載

--- ogg/clear.sh
+++ ogg/clear.sh
@@ -1,16 +1,41 @@
 #!/bin/bash
+docker-compose -f docker-compose.yml down -v
 docker-compose -f docker-compose-19ca.yml down -v
 docker-compose -f docker-compose-19ma.yml down -v
 docker-compose -f docker-compose-21ma.yml down -v
+
 items=(
-    "docker/ogg19c-ca/data/source-ogg"
-    "docker/ogg19c-ca/data/target-ogg"
-    "docker/ogg19c-ma/data/source-deployments"
-    "docker/ogg19c-ma/data/target-deployments"
-    "docker/ogg21c-ma/data/source-deployments"
-    "docker/ogg21c-ma/data/target-deployments"
-    "docker/oracle19c/data/source-oradata"
-    "docker/oracle19c/data/target-oradata"
+    "docker/ogg19c-ca/data"
+    "docker/ogg19c-ma/data"
+    "docker/ogg21c-ma/data"
+    "docker/oracle19c/data"
+)
+for item in "${items[@]}" ; do
+    if [ -e $item ]; then
+        rm -rf $item
+    fi
+    mkdir -p $item
+    chown 1000:1000 $item
+    chmod 775 $item
+done
+
+items=(
+    "docker/ogg19c-ca/data/source"
+    "docker/ogg19c-ca/data/source-19ca"
+    "docker/ogg19c-ca/data/target-19ca"
+    "docker/ogg19c-ma/data/source-19ma"
+    "docker/ogg19c-ma/data/target-19ma"
+    "docker/ogg21c-ma/data/source-21ma"
+    "docker/ogg21c-ma/data/target"
+    "docker/ogg21c-ma/data/target-21ma"
+    "docker/oracle19c/data/source"
+    "docker/oracle19c/data/source-19ca"
+    "docker/oracle19c/data/source-19ma"
+    "docker/oracle19c/data/source-21ma"
+    "docker/oracle19c/data/target"
+    "docker/oracle19c/data/target-19ca"
+    "docker/oracle19c/data/target-19ma"
+    "docker/oracle19c/data/target-21ma"
 )
 for item in "${items[@]}" ; do
     if [ -e $item ]; then
